f3c84180971356c537213dc883429264b3a7765b
derrick b <dbozich1@gmail.com>
2018-08-01 04:23:27 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -76,7 +76,11 @@ class SongsController < ApplicationController
 
     @song = Song.find_by_id(params['song_id'])
     @song.artist = Artist.find_or_create_by(:name => params["artist_name"])
-    @song.genre_ids = params['genres']
+    params['genres'].each do |g|
+      genre = Genre.find_or_create_by(:id => g)
+
+      @song.genres << genre
+    end
     @song.save
 
 
